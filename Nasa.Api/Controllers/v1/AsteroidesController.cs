591dee5f7fa05a6a8b250f02cf17e99e86d0f9f4
Enrique Fernandez <enriquefernandezchacon@gmail.com>
2022-12-20 17:33:41 +0100

    Inicio Configuracion Swagger

--- Nasa.Api/Controllers/v1/AsteroidesController.cs
+++ Nasa.Api/Controllers/v1/AsteroidesController.cs
@@ -1,8 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Nasa.Api.Mapper;
 using Nasa.Api.Services;
+using Nasa.Data.Models;
 
-namespace Nasa.Api.Controllers
+namespace Nasa.Api.Controllers.v1
 {
     [ApiController]
     [Route("api/[controller]")]
@@ -19,12 +20,21 @@ namespace Nasa.Api.Controllers
             _asteroidesMapper = asteroidesMapper;
         }
 
-        [HttpGet("{dias}")]
+        // POST: api/v1/Asteroides/asteroidesenelfuturo/3
+        /// <summary>
+        /// Obtiene los 3 asteroides con mas peligro para la tierra desde hoy hasta la fecha indicada mediante los dias indicados
+        /// por parametro
+        /// </summary>
+        /// <param name="dias"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("asteroidesenelfuturo/{dias}")]
+        [ProducesResponseType(typeof(List<AsteroideDto>), 200)]
         public IActionResult Get(string dias)
         {
             int valorDia;
 
-            if (!Int32.TryParse(dias, out valorDia))
+            if (!int.TryParse(dias, out valorDia))
             {
                 return BadRequest("El valor de dias no es correcto");
             }
@@ -33,7 +43,7 @@ namespace Nasa.Api.Controllers
             {
                 return BadRequest("El valor de dias debe estar entre 1 y 7");
             }
-            
+
             var listaAsteroides = _asteroidesService.GetAsteroides(valorDia, 3);
 
             if (listaAsteroides == null)
--- Nasa.Console/Models/AsteroideDto.Model.cs
+++ Nasa.Console/Models/AsteroideDto.Model.cs
@@ -2,7 +2,13 @@
 {
     public class AsteroideDto
     {
+        /// <summary> 
+        /// Employee id 
+        /// </summary> 
         public string Nombre { get; set; }
+        /// <summary> 
+        /// Employee id 2
+        /// </summary> 
         public Double Diametro { get; set; }
         public Double Velocidad { get; set; }
         public DateTimeOffset FechaAproximacion { get; set; }
