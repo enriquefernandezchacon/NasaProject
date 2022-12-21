bb52296a750e1dadc7fe2378c66afe3784bdf066
Enrique Fernandez <enriquefernandezchacon@gmail.com>
2022-12-21 10:09:42 +0100

    Refactorizacion ConsoleApp -> Biblioteca

---
+++ Nasa.Data/Models/AsteroideDto.Model.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace Nasa.Data.Models
+{
+    public class AsteroideDto
+    {
+        /// <summary> 
+        /// Nombre del Asteroide
+        /// </summary> 
+        /// 
+        [Required]
+        public string Nombre { get; set; }
+
+        /// <summary> 
+        /// Diamentro del Asteroide (m)
+        /// </summary> 
+        [Required]
+        public Double Diametro { get; set; }
+
+        /// <summary> 
+        /// Velocidad del Asteroide (km/h)
+        /// </summary> 
+        [Required]
+        public Double Velocidad { get; set; }
+
+        /// <summary> 
+        /// Fecha de la aproximación al planeta señalado
+        /// </summary> 
+        [Required]
+        public DateTimeOffset FechaAproximacion { get; set; }
+
+        /// <summary> 
+        /// Planeta al que se aproxima
+        /// </summary> 
+        [Required]
+        public string Planeta { get; set; }
+    }
+}
